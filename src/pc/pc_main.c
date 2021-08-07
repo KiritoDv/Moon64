6da97fa2c0356e3de773871849132f4171e857d2
KiritoDev <nohomoboi01@gmail.com>
2021-08-07 16:31:58 -0500

    Moved dev to experiments

--- src/pc/pc_main.c
+++ src/pc/pc_main.c
@@ -1,11 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#ifdef TARGET_WEB
-#include <emscripten.h>
-#include <emscripten/html5.h>
-#endif
-
 #include "sm64.h"
 
 #include "game/memory.h"
@@ -64,11 +59,8 @@ extern void thread5_game_loop(void *arg);
 extern void create_next_audio_buffer(s16 *samples, u32 num_samples);
 void game_loop_one_iteration(void);
 
-void dispatch_audio_sptask(struct SPTask *spTask) {
-}
-
-void set_vblank_handler(s32 index, struct VblankHandler *handler, OSMesgQueue *queue, OSMesg *msg) {
-}
+void dispatch_audio_sptask(struct SPTask *spTask) {}
+void set_vblank_handler(s32 index, struct VblankHandler *handler, OSMesgQueue *queue, OSMesg *msg) {}
 
 static bool inited = false;
 
@@ -78,13 +70,8 @@ void send_display_list(struct SPTask *spTask) {
     gfx_run((Gfx *)spTask->task.t.data_ptr);
 }
 
-#ifdef VERSION_EU
-#define SAMPLES_HIGH 656
-#define SAMPLES_LOW 640
-#else
 #define SAMPLES_HIGH 544
 #define SAMPLES_LOW 528
-#endif
 
 void produce_one_frame(void) {
     moon_setup("Update");
@@ -143,44 +130,8 @@ void game_deinit(void) {
 
 void game_exit(void) {
     game_deinit();
-#ifndef TARGET_WEB
     exit(0);
-#endif
 }
-
-#ifdef TARGET_WEB
-static void em_main_loop(void) {
-}
-
-static void request_anim_frame(void (*func)(double time)) {
-    EM_ASM(requestAnimationFrame(function(time) {
-        dynCall("vd", $0, [time]);
-    }), func);
-}
-
-static void on_anim_frame(double time) {
-    static double target_time;
-
-    time *= 0.03; // milliseconds to frame count (33.333 ms -> 1)
-
-    if (time >= target_time + 10.0) {
-        // We are lagging 10 frames behind, probably due to coming back after inactivity,
-        // so reset, with a small margin to avoid potential jitter later.
-        target_time = time - 0.010;
-    }
-
-    for (int i = 0; i < 2; i++) {
-        // If refresh rate is 15 Hz or something we might need to generate two frames
-        if (time >= target_time) {
-            produce_one_frame();
-            target_time = target_time + 1.0;
-        }
-    }
-
-    if (inited) // only continue if the init flag is still set
-        request_anim_frame(on_anim_frame);
-}
-#endif
 
 void main_func(char *argv[]) {
 
@@ -238,17 +189,12 @@ void main_func(char *argv[]) {
     discord_init();
 #endif
 
-#ifdef TARGET_WEB
-    emscripten_set_main_loop(em_main_loop, 0, 0);
-    request_anim_frame(on_anim_frame);
-#else
     while (true) {
         wm_api->main_loop(produce_one_frame);
 #ifdef DISCORDRPC
         discord_update_rich_presence();
 #endif
     }
-#endif
 }
 
 int main(int argc, char *argv[]) {
